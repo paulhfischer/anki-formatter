e937c1d7cfc1b8caafefd2362b8b97817bdeeba2
Paul Fischer <paul.fischer@mailbox.org>
2023-12-07 12:14:04 +0100

    added occlusion formatter

---
+++ src/anki_formatter/formatters/occlusion.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import re
+
+from anki_formatter.formatters.plaintext import convert_to_plaintext
+
+
+def format_occlusion(value: str) -> tuple[str, bool]:
+    formatted_value, _ = convert_to_plaintext(value)
+
+    clozes = [
+        (int(match[0]), str(match[1]))
+        for match in re.findall(r"\{\{c(\d+)::(.+?)\}\}", formatted_value)
+    ]
+    clozes = sorted(clozes, key=lambda x: x[0])
+    formatted_value = "".join(f"{{{{c{cloze_id}::{image_id}}}}}" for cloze_id, image_id in clozes)
+
+    return formatted_value, value != formatted_value
