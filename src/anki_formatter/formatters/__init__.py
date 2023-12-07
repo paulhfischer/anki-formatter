af3a75a9f279c9a28e422bd2acb3eb42b8eeafcd
Paul Fischer <paul.fischer@mailbox.org>
2023-12-07 12:14:04 +0100

    added occlusion formatter

--- src/anki_formatter/formatters/__init__.py
+++ src/anki_formatter/formatters/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from anki_formatter.formatters.clear import clear
 from anki_formatter.formatters.html import format_html
 from anki_formatter.formatters.image_occlusion_svg import format_image_occlusion_field
+from anki_formatter.formatters.occlusion import format_occlusion
 from anki_formatter.formatters.plaintext import convert_to_plaintext
 from anki_formatter.formatters.skip import skip
 
@@ -11,5 +12,6 @@ FORMATTERS = {
     "plaintext": convert_to_plaintext,
     "html": format_html,
     "skip": skip,
+    "occlusion": format_occlusion,
     "imageOcclusionSVG": format_image_occlusion_field,
 }
