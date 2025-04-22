1b0b715e785e6b01a525d725c5f3cca5e64aea10
Paul Fischer <paul.fischer@mailbox.org>
2025-04-22 12:27:23 +0200

    refactor formatter tests

--- tests/formatter_test.py
+++ tests/formatter_test.py
@@ -30,7 +30,10 @@ def mocked_links_requests(url: str, timeout: int | None = None) -> Mock:
 
 @pytest.mark.parametrize(
     ("input", "expected_output"),
-    (("foobar", ""),),
+    (
+        ("", ""),
+        ("foobar", ""),
+    ),
 )
 def test_clear_formatter(input: str, expected_output: str) -> None:
     ret_1, _ = clear(input, False)
@@ -92,142 +95,346 @@ def test_occlusion_formatter(input: str, expected_output: str) -> None:
 @pytest.mark.parametrize(
     ("input", "expected_output"),
     (
-        ("foobar", "foobar"),
-        ("line1<br>line2", "line1<br>\nline2"),
-        ("<img src='foo.jpg'><br>bar", "<img src='foo.jpg'><br>\nbar"),
-        ("<li>foo<br>bar</li>", "<li>foo<br>bar</li>"),
-        ("<li>foo<br></li>", "<li>foo</li>"),
-        ("<div>text</div>", "text"),
-        ("<ul><li>foo</li><li>bar</li></ul>", "<ul>\n  <li>foo</li>\n  <li>bar</li>\n</ul>"),
         (
-            "<ul><li>foo</li><ul><li>foo</li><li>bar</li></ul></ul>",
-            "<ul>\n  <li>foo</li>\n  <ul>\n    <li>foo</li>\n    <li>bar</li>\n  </ul>\n</ul>",
+            """foobar""",
+            """foobar""",
         ),
         (
-            "text with <b>important</b> part.",
-            "text with <b>important</b> part.",
+            """line1<br>line2""",
+            """line1<br>\nline2""",
         ),
         (
-            "<section><img src='foo.jpg'><img src='foo.jpg'></section>",
-            "<section>\n  <img src='foo.jpg'>\n  <img src='foo.jpg'>\n</section>",
+            """<img src='foo.jpg'><br>bar""",
+            """<img src='foo.jpg'><br>\nbar""",
         ),
         (
-            "<section>text <b>with important</b> part!</section>",
-            "<section>\n  text <b>with important</b> part!\n</section>",
+            """<li>foo<br>bar</li>""",
+            """<li>foo<br>bar</li>""",
         ),
         (
-            "<section><img src='foo.jpg'><br><img src='bar.jpg'></section>",
-            "<section>\n  <img src='foo.jpg'><br>\n  <img src='bar.jpg'>\n</section>",
+            """<li>foo<br></li>""",
+            """<li>foo</li>""",
         ),
         (
-            "<section><img src='foo.jpg'><br><b>important</b> info</section>",
-            "<section>\n  <img src='foo.jpg'><br>\n  <b>important</b> info\n</section>",
-        ),
-        ("<li><b>foo</b> bar</li>", "<li><b>foo</b> bar</li>"),
-        ("<li><b>a</b> foo\nbar</li>", "<li><b>a</b> foo bar</li>"),
-        ("<b>foo</b>?", "<b>foo</b>?"),
-        ("{{c1:: <b>foo</b> }} bar", "{{c1::<b>foo</b>}} bar"),
-        ("{{c1::<b>foo</b>}} bar", "{{c1::<b>foo</b>}} bar"),
-        ("<b>foo</b><b>bar</b>", "<b>foobar</b>"),
-        ("<b>foo</b> <b>bar</b>", "<b>foo</b> <b>bar</b>"),
-        ("A <sub>B</sub> C", "A<sub>B</sub> C"),
-        ("A<sub>B</sub> C", "A<sub>B</sub> C"),
-        ("A<sub>B</sub>-C", "A<sub>B</sub>-C"),
-        ("<li><b>foo</b></li>", "<li><b>foo</b></li>"),
-        ("<li>foobar bar<sub>foo</sub></li>", "<li>foobar bar<sub>foo</sub></li>"),
-        ("<li>foo<sub>bar</sub></li>", "<li>foo<sub>bar</sub></li>"),
-        ("<li>foo <sub>bar</sub></li>", "<li>foo<sub>bar</sub></li>"),
-        ("<li>foo<sub>bar</sub> </li>", "<li>foo<sub>bar</sub></li>"),
-        ("<li>foo <sub>bar</sub> </li>", "<li>foo<sub>bar</sub></li>"),
-        ("„foo“", '"foo"'),
-        ("<img src='„foo“'>", "<img src='„foo“'>"),
-        ("<li>„foo“</li>", '<li>"foo"</li>'),
-        ("<li>foo</li><li></li>", "<li>foo</li>"),
-        ("<ul>\n  <li>foo</li>\n  <li>\n    \n  </li>\n</ul>", "<ul>\n  <li>foo</li>\n</ul>"),
-        ("<li>foo =&gt; foo&nbsp;bar</li>", "<li>foo ⇒ foo bar</li>"),
-        ("<b>foo</b>  <b>bar</b>", "<b>foo</b> <b>bar</b>"),
-        ("<b>foo </b><b>bar</b>", "<b>foo</b> <b>bar</b>"),
-        (
-            "<b>foo </b>\n<b>bar</b>",
-            "<b>foo</b> <b>bar</b>",
-        ),
-        ("<b>foo</b>bar", "<b>foo</b>bar"),
-        ("<b>foo</b>-bar", "<b>foo</b>-bar"),
-        ("<b>foo</b> -bar", "<b>foo</b> -bar"),
-        ("foo<b>bar</b>", "foo<b>bar</b>"),
-        ("foo-<b>bar</b>", "foo-<b>bar</b>"),
-        ("foo- <b>bar</b>", "foo- <b>bar</b>"),
-        ("foo<br><b>bar</b>", "foo<br>\n<b>bar</b>"),
-        ("foo<br> <b>bar</b>", "foo<br>\n<b>bar</b>"),
-        (
-            "<section>foo<br><b>bar</b></section>",
-            "<section>\n  foo<br>\n  <b>bar</b>\n</section>",
+            """<div>text</div>""",
+            """text""",
         ),
         (
-            "<section>foo<br> <b>bar</b></section>",
-            "<section>\n  foo<br>\n  <b>bar</b>\n</section>",
+            """<ul><li>foo</li><li>bar</li></ul>""",
+            """<ul>\n  <li>foo</li>\n  <li>bar</li>\n</ul>""",
         ),
-        ("foo&nbsp;<b>bar</b>", "foo <b>bar</b>"),
-        ("foo<b>&nbsp;bar</b>", "foo <b>bar</b>"),
-        ("foo<sub>bar</sub>&nbsp;foo", "foo<sub>bar</sub> foo"),
-        ("foo <i>bar</i> foobar", "foo <i>bar</i> foobar"),
-        ("<strong>foobar</strong>", "<b>foobar</b>"),
-        ("<em>foobar</em>", "<i>foobar</i>"),
-        ("foo <strong>foobar</strong> bar", "foo <b>foobar</b> bar"),
-        ("<ol>\n  <li>foo</li>\n</ol>", "<ol>\n  <li>foo</li>\n</ol>"),
-        ("<ol start='2'>\n  <li>foo</li>\n</ol>", "<ol start='2'>\n  <li>foo</li>\n</ol>"),
-        ("NAD⁺ + Pᵢ", "NAD<sup>+</sup> + P<sub>i</sub>"),
-        (r"\[A <=> B\]", r"\[A <=> B\]"),
-        (r"\(A <=> B\)", r"\(A <=> B\)"),
-        (r"foo <=> bar \[A <=> B\]", r"foo ⇔ bar \[A <=> B\]"),
-        (r"foo <=> bar \(A <=> B\)", r"foo ⇔ bar \(A <=> B\)"),
-        (r"\[A <=> B\] foo <=> bar", r"\[A <=> B\] foo ⇔ bar"),
-        (r"\(A <=> B\) foo <=> bar", r"\(A <=> B\) foo ⇔ bar"),
-        (r"foo <=> bar \[A <=> B\] foo <=> bar", r"foo ⇔ bar \[A <=> B\] foo ⇔ bar"),
-        (r"foo <=> bar \(A <=> B\) foo <=> bar", r"foo ⇔ bar \(A <=> B\) foo ⇔ bar"),
-        ("<b>foo</b>&nbsp;<i>bar</i>", "<b>foo</b> <i>bar</i>"),
-        ("<li><b>foo</b><br></li>", "<li><b>foo</b></li>"),
-        ("<u>foo<b>bas</b>    bar</u>", "<u>foo<b>bas</b> bar</u>"),
-        ("<u>foo   bar</u>", "<u>foo bar</u>"),
-        ("<u>foo <u>bar</u></u>", "<u>foo bar</u>"),
-        ("<u>foo <ins>bar</ins></u>", "<u>foo <ins>bar</ins></u>"),
-        ("<u>foo <u>bar</u> <b>bas</b></u>", "<u>foo bar <b>bas</b></u>"),
-        ("<u>foo<b>bas</b> <u>bar</u></u>", "<u>foo<b>bas</b> bar</u>"),
-        ("<u>foo<b>bas</b>  <u>bar</u></u>", "<u>foo<b>bas</b> bar</u>"),
-        ("<u>foo<b>bas</b> <u> bar</u></u>", "<u>foo<b>bas</b> bar</u>"),
-        ("<u>foo<b>bas </b> <b>  bar</b></u>", "<u>foo<b>bas</b> <b>bar</b></u>"),
-        ("<u>foo<b>bas </b> <u>  bar</u></u>", "<u>foo<b>bas</b> bar</u>"),
-        ("<li>foo<br>bar</li>", "<li>foo<br>bar</li>"),
-        ("<li><u>foo</u><u>bar</u></li>", "<li><u>foobar</u></li>"),
-        ("<li><u>foo</u><b>bar</b></li>", "<li><u>foo</u><b>bar</b></li>"),
-        ("<li><u>foo</u><br><u>bar</u></li>", "<li><u>foo</u><br><u>bar</u></li>"),
-        ("<li><u>foo</u><br><b>bar</b></li>", "<li><u>foo</u><br><b>bar</b></li>"),
         (
-            '<table style="border-collapse: collapse"><caption>foobar</caption><colgroup><col style="width: 20%"><col style="width: 80%; height: 32px"></colgroup><tbody><tr><td style="text-align: center;">foo</td><td style="text-align: center;">bar</td></tr><tr><td>foo<br>bar</td><td>foo<br>bar</td></tr><tr><td><u>foo</u><br><u>bar</u></td><td><u>foo</u><br><b>bar</b></td></tr></tbody></table>',  # noqa: E501
-            """\
-<table border='1' style='border-collapse: collapse;'>
-  <caption>foobar</caption>
-  <colgroup>
-    <col style='width: 20%;'>
-    <col style='width: 80%;'>
-  </colgroup>
-  <tbody>
-    <tr>
-      <td style='text-align: center;'>foo</td>
-      <td style='text-align: center;'>bar</td>
-    </tr>
-    <tr>
-      <td>foo<br>bar</td>
-      <td>foo<br>bar</td>
-    </tr>
-    <tr>
-      <td><u>foo</u><br><u>bar</u></td>
-      <td><u>foo</u><br><b>bar</b></td>
-    </tr>
-  </tbody>
-</table>""",
+            """<ul><li>foo</li><ul><li>foo</li><li>bar</li></ul></ul>""",
+            """<ul>\n  <li>foo</li>\n  <ul>\n    <li>foo</li>\n    <li>bar</li>\n  </ul>\n</ul>""",
         ),
-        ("<td style='foo: bar'>foobar</td>", "<td>foobar</td>"),
+        (
+            """text with <b>important</b> part.""",
+            """text with <b>important</b> part.""",
+        ),
+        (
+            """<section><img src='foo.jpg'><img src='foo.jpg'></section>""",
+            """<section>\n  <img src='foo.jpg'>\n  <img src='foo.jpg'>\n</section>""",
+        ),
+        (
+            """<section>text <b>with important</b> part!</section>""",
+            """<section>\n  text <b>with important</b> part!\n</section>""",
+        ),
+        (
+            """<section><img src='foo.jpg'><br><img src='bar.jpg'></section>""",
+            """<section>\n  <img src='foo.jpg'><br>\n  <img src='bar.jpg'>\n</section>""",
+        ),
+        (
+            """<section><img src='foo.jpg'><br><b>important</b> info</section>""",
+            """<section>\n  <img src='foo.jpg'><br>\n  <b>important</b> info\n</section>""",
+        ),
+        (
+            """<li><b>foo</b> bar</li>""",
+            """<li><b>foo</b> bar</li>""",
+        ),
+        (
+            """<li><b>a</b> foo\nbar</li>""",
+            """<li><b>a</b> foo bar</li>""",
+        ),
+        (
+            """<b>foo</b>?""",
+            """<b>foo</b>?""",
+        ),
+        (
+            """{{c1:: <b>foo</b> }} bar""",
+            """{{c1::<b>foo</b>}} bar""",
+        ),
+        (
+            """{{c1::<b>foo</b>}} bar""",
+            """{{c1::<b>foo</b>}} bar""",
+        ),
+        (
+            """<b>foo</b><b>bar</b>""",
+            """<b>foobar</b>""",
+        ),
+        (
+            """<b>foo</b> <b>bar</b>""",
+            """<b>foo</b> <b>bar</b>""",
+        ),
+        (
+            """A <sub>B</sub> C""",
+            """A<sub>B</sub> C""",
+        ),
+        (
+            """A<sub>B</sub> C""",
+            """A<sub>B</sub> C""",
+        ),
+        (
+            """A<sub>B</sub>-C""",
+            """A<sub>B</sub>-C""",
+        ),
+        (
+            """<li><b>foo</b></li>""",
+            """<li><b>foo</b></li>""",
+        ),
+        (
+            """<li>foobar bar<sub>foo</sub></li>""",
+            """<li>foobar bar<sub>foo</sub></li>""",
+        ),
+        (
+            """<li>foo<sub>bar</sub></li>""",
+            """<li>foo<sub>bar</sub></li>""",
+        ),
+        (
+            """<li>foo <sub>bar</sub></li>""",
+            """<li>foo<sub>bar</sub></li>""",
+        ),
+        (
+            """<li>foo<sub>bar</sub> </li>""",
+            """<li>foo<sub>bar</sub></li>""",
+        ),
+        (
+            """<li>foo <sub>bar</sub> </li>""",
+            """<li>foo<sub>bar</sub></li>""",
+        ),
+        (
+            """„foo“""",
+            """\"foo\"""",
+        ),
+        (
+            """<img src='„foo“'>""",
+            """<img src='„foo“'>""",
+        ),
+        (
+            """<li>„foo“</li>""",
+            """<li>"foo"</li>""",
+        ),
+        (
+            """<li>foo</li><li></li>""",
+            """<li>foo</li>""",
+        ),
+        (
+            """<ul>\n  <li>foo</li>\n  <li>\n    \n  </li>\n</ul>""",
+            """<ul>\n  <li>foo</li>\n</ul>""",
+        ),
+        (
+            """<li>foo =&gt; foo&nbsp;bar</li>""",
+            """<li>foo ⇒ foo bar</li>""",
+        ),
+        (
+            """<b>foo</b>  <b>bar</b>""",
+            """<b>foo</b> <b>bar</b>""",
+        ),
+        (
+            """<b>foo </b><b>bar</b>""",
+            """<b>foo</b> <b>bar</b>""",
+        ),
+        (
+            """<b>foo </b>\n<b>bar</b>""",
+            """<b>foo</b> <b>bar</b>""",
+        ),
+        (
+            """<b>foo</b>bar""",
+            """<b>foo</b>bar""",
+        ),
+        (
+            """<b>foo</b>-bar""",
+            """<b>foo</b>-bar""",
+        ),
+        (
+            """<b>foo</b> -bar""",
+            """<b>foo</b> -bar""",
+        ),
+        (
+            """foo<b>bar</b>""",
+            """foo<b>bar</b>""",
+        ),
+        (
+            """foo-<b>bar</b>""",
+            """foo-<b>bar</b>""",
+        ),
+        (
+            """foo- <b>bar</b>""",
+            """foo- <b>bar</b>""",
+        ),
+        (
+            """foo<br><b>bar</b>""",
+            """foo<br>\n<b>bar</b>""",
+        ),
+        (
+            """foo<br> <b>bar</b>""",
+            """foo<br>\n<b>bar</b>""",
+        ),
+        (
+            """<section>foo<br><b>bar</b></section>""",
+            """<section>\n  foo<br>\n  <b>bar</b>\n</section>""",
+        ),
+        (
+            """<section>foo<br> <b>bar</b></section>""",
+            """<section>\n  foo<br>\n  <b>bar</b>\n</section>""",
+        ),
+        (
+            """foo&nbsp;<b>bar</b>""",
+            """foo <b>bar</b>""",
+        ),
+        (
+            """foo<b>&nbsp;bar</b>""",
+            """foo <b>bar</b>""",
+        ),
+        (
+            """foo<sub>bar</sub>&nbsp;foo""",
+            """foo<sub>bar</sub> foo""",
+        ),
+        (
+            """foo <i>bar</i> foobar""",
+            """foo <i>bar</i> foobar""",
+        ),
+        (
+            """<strong>foobar</strong>""",
+            """<b>foobar</b>""",
+        ),
+        (
+            """<em>foobar</em>""",
+            """<i>foobar</i>""",
+        ),
+        (
+            """foo <strong>foobar</strong> bar""",
+            """foo <b>foobar</b> bar""",
+        ),
+        (
+            """<ol>\n  <li>foo</li>\n</ol>""",
+            """<ol>\n  <li>foo</li>\n</ol>""",
+        ),
+        (
+            """<ol start='2'>\n  <li>foo</li>\n</ol>""",
+            """<ol start='2'>\n  <li>foo</li>\n</ol>""",
+        ),
+        (
+            """NAD⁺ + Pᵢ""",
+            """NAD<sup>+</sup> + P<sub>i</sub>""",
+        ),
+        (
+            r"""\[A <=> B\]""",
+            r"""\[A <=> B\]""",
+        ),
+        (
+            r"""\(A <=> B\)""",
+            r"""\(A <=> B\)""",
+        ),
+        (
+            r"""foo <=> bar \[A <=> B\]""",
+            r"""foo ⇔ bar \[A <=> B\]""",
+        ),
+        (
+            r"""foo <=> bar \(A <=> B\)""",
+            r"""foo ⇔ bar \(A <=> B\)""",
+        ),
+        (
+            r"""\[A <=> B\] foo <=> bar""",
+            r"""\[A <=> B\] foo ⇔ bar""",
+        ),
+        (
+            r"""\(A <=> B\) foo <=> bar""",
+            r"""\(A <=> B\) foo ⇔ bar""",
+        ),
+        (
+            r"""foo <=> bar \[A <=> B\] foo <=> bar""",
+            r"""foo ⇔ bar \[A <=> B\] foo ⇔ bar""",
+        ),
+        (
+            r"""foo <=> bar \(A <=> B\) foo <=> bar""",
+            r"""foo ⇔ bar \(A <=> B\) foo ⇔ bar""",
+        ),
+        (
+            """<b>foo</b>&nbsp;<i>bar</i>""",
+            """<b>foo</b> <i>bar</i>""",
+        ),
+        (
+            """<li><b>foo</b><br></li>""",
+            """<li><b>foo</b></li>""",
+        ),
+        (
+            """<u>foo<b>bas</b>    bar</u>""",
+            """<u>foo<b>bas</b> bar</u>""",
+        ),
+        (
+            """<u>foo   bar</u>""",
+            """<u>foo bar</u>""",
+        ),
+        (
+            """<u>foo <u>bar</u></u>""",
+            """<u>foo bar</u>""",
+        ),
+        (
+            """<u>foo <ins>bar</ins></u>""",
+            """<u>foo <ins>bar</ins></u>""",
+        ),
+        (
+            """<u>foo <u>bar</u> <b>bas</b></u>""",
+            """<u>foo bar <b>bas</b></u>""",
+        ),
+        (
+            """<u>foo<b>bas</b> <u>bar</u></u>""",
+            """<u>foo<b>bas</b> bar</u>""",
+        ),
+        (
+            """<u>foo<b>bas</b>  <u>bar</u></u>""",
+            """<u>foo<b>bas</b> bar</u>""",
+        ),
+        (
+            """<u>foo<b>bas</b> <u> bar</u></u>""",
+            """<u>foo<b>bas</b> bar</u>""",
+        ),
+        (
+            """<u>foo<b>bas </b> <b>  bar</b></u>""",
+            """<u>foo<b>bas</b> <b>bar</b></u>""",
+        ),
+        (
+            """<u>foo<b>bas </b> <u>  bar</u></u>""",
+            """<u>foo<b>bas</b> bar</u>""",
+        ),
+        (
+            """<li>foo<br>bar</li>""",
+            """<li>foo<br>bar</li>""",
+        ),
+        (
+            """<li><u>foo</u><u>bar</u></li>""",
+            """<li><u>foobar</u></li>""",
+        ),
+        (
+            """<li><u>foo</u><b>bar</b></li>""",
+            """<li><u>foo</u><b>bar</b></li>""",
+        ),
+        (
+            """<li><u>foo</u><br><u>bar</u></li>""",
+            """<li><u>foo</u><br><u>bar</u></li>""",
+        ),
+        (
+            """<li><u>foo</u><br><b>bar</b></li>""",
+            """<li><u>foo</u><br><b>bar</b></li>""",
+        ),
+        (
+            """<table style="border-collapse: collapse"><caption>foobar</caption><colgroup><col style="width: 20%"><col style="width: 80%; height: 32px"></colgroup><tbody><tr><td style="text-align: center;">foo</td><td style="text-align: center;">bar</td></tr><tr><td>foo<br>bar</td><td>foo<br>bar</td></tr><tr><td><u>foo</u><br><u>bar</u></td><td><u>foo</u><br><b>bar</b></td></tr></tbody></table>""",  # noqa: E501
+            """<table border='1' style='border-collapse: collapse;'>\n  <caption>foobar</caption>\n  <colgroup>\n    <col style='width: 20%;'>\n    <col style='width: 80%;'>\n  </colgroup>\n  <tbody>\n    <tr>\n      <td style='text-align: center;'>foo</td>\n      <td style='text-align: center;'>bar</td>\n    </tr>\n    <tr>\n      <td>foo<br>bar</td>\n      <td>foo<br>bar</td>\n    </tr>\n    <tr>\n      <td><u>foo</u><br><u>bar</u></td>\n      <td><u>foo</u><br><b>bar</b></td>\n    </tr>\n  </tbody>\n</table>""",  # noqa: E501
+        ),
+        (
+            """<td style='foo: bar'>foobar</td>""",
+            """<td>foobar</td>""",
+        ),
     ),
 )
 def test_html_formatter(input: str, expected_output: str) -> None:
